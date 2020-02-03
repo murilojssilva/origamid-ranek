b9c3d0f80fd221f6ff71d79985795e5bdc0c083f
murilojssilva <murilojssilva>
2020-02-03 11:42:50 -0300

    Get produtos with fetch

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,9 +1,33 @@
 <template>
-  <p></p>
+  <section class="produtos-container">
+    <div v-for="produto in produtos" :key="produto.id">
+      <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo" />
+      <p class="preco">{{produto.preco}}</p>
+      <h2 class="titulo">{{produto.nome}}</h2>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      produtos: null
+    };
+  },
+  methods: {
+    getProdutos() {
+      fetch("http://localhost:3000/produto")
+        .then(response => response.json())
+        .then(response => {
+          this.produtos = response;
+        });
+    }
+  },
+  created() {
+    this.getProdutos();
+  }
+};
 </script>
 
 <style>
