d574a5c0abe2941c158330219fc13438c8854db1
murilojssilva <murilojssilva>
2020-02-03 12:11:26 -0300

    Get produtos with axios

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,14 +1,19 @@
 <template>
   <section class="produtos-container">
     <div v-for="produto in produtos" :key="produto.id">
-      <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo" />
-      <p class="preco">{{produto.preco}}</p>
-      <h2 class="titulo">{{produto.nome}}</h2>
+      <img
+        v-if="produto.fotos"
+        :src="produto.fotos[0].src"
+        :alt="produto.fotos[0].titulo"
+      />
+      <p class="preco">{{ produto.preco }}</p>
+      <h2 class="titulo">{{ produto.nome }}</h2>
     </div>
   </section>
 </template>
 
 <script>
+import { api } from "@/services.js";
 export default {
   data() {
     return {
@@ -17,11 +22,9 @@ export default {
   },
   methods: {
     getProdutos() {
-      fetch("http://localhost:3000/produto")
-        .then(response => response.json())
-        .then(response => {
-          this.produtos = response;
-        });
+      api.get("/produto").then(response => {
+        this.produtos = response.data;
+      });
     }
   },
   created() {
@@ -30,5 +33,4 @@ export default {
 };
 </script>
 
-<style>
-</style>
+<style></style>
