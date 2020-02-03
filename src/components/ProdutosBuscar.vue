99bbde0b68f264f479e879f0d21aed1332cf646b
murilojssilva <murilojssilva>
2020-02-03 13:53:10 -0300

    Update ProdutosLista

--- src/components/ProdutosBuscar.vue
+++ src/components/ProdutosBuscar.vue
@@ -1,10 +1,29 @@
 <template>
-  <input type="text" />
+  <form action="">
+    <input type="text" name="busca" v-model="busca" id="busca" />
+    <input
+      type="submit"
+      id="lupa"
+      value="buscar"
+      @click.prevent="buscarProdutos"
+    />
+    {{ busca }}
+  </form>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      busca: ""
+    };
+  },
+  methods: {
+    buscarProdutos() {
+      this.$router.push({ query: { q: this.busca } });
+    }
+  }
+};
 </script>
 
-<style>
-</style>
+<style></style>
--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -8,23 +8,38 @@
       />
       <p class="preco">{{ produto.preco }}</p>
       <h2 class="titulo">{{ produto.nome }}</h2>
+      <p>{{ produto.descricao }}</p>
     </div>
   </section>
 </template>
 
 <script>
 import { api } from "@/services.js";
+import { serialize } from "@/helpers.js";
+
 export default {
   data() {
     return {
-      produtos: null
+      produtos: null,
+      produtosPorPagina: 9
     };
+  },
+  computed: {
+    url() {
+      const query = serialize(this.$route.query);
+      return `/produto?_limit=${this.produtosPorPagina}${query}`;
+    }
   },
   methods: {
     getProdutos() {
-      api.get("/produto").then(response => {
+      api.get(this.url).then(response => {
         this.produtos = response.data;
       });
+    }
+  },
+  watch: {
+    url() {
+      this.getProdutos();
     }
   },
   created() {
